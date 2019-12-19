e8c9c994f9fd0ed2305a17f9d3450c939a33b57b
Gisele <giskey11@gmail.com>
2019-12-18 23:01:44 -0500

    Add Js caracters

--- scrypt.js
+++ scrypt.js
@@ -1,46 +1,23 @@
-//Set funcion for lenght
+//Password Length input
+var passeworLength = document.getElementById("passewordLength")
 
-var btng = document.getElementById("generatepw")
-var generatePassword = function(){
-    // alert("salut")
-    var passewordLenght = prompt("Enter your passeword lenght")
-    var passewordLenghtinnumber = parseInt(passewordLenght)
-console.log (passewordLenght )
-console.log(passewordLenghtinnumber)
-if (isNaN(passewordLenghtinnumber)) {
-    alert("This is not a number")
-}else {
-    if (passewordLenghtinnumber < 8  ){
-        alert("the number is too low")
-    }
-    else if(passewordLenghtinnumber >128){
-    alert("the number is too hight")
+//checkBox nbLowerCaseChar
+var nbLowerCaseChar = document.getElementById("nbLowerCaseChar")
+var upperCaseChar = document.getElementById("upperCaseChar")
+var numericalChar = document.getElementById("numericalChar")
+var specialChar = document.getElementById("specialChar")
+var inputPasseword = document.getElementById("inputPasseword")
 
-        }
-    
-}
-}
- btng.addEventListener("click",generatePassword)
+//buttons
+var btnGeneratepw = document.getElementById("generatepw")
+var btnCopyPassword = document.getElementById("copyPassword")
 
-//Generate random passeword
-function generatepw(){
-    let complexity = document.getElementById("slider").nodeValue;
-    let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrtuvwxyz1234567890!@$%^&*()_+"
-    let passeword = "";
-}
+//Caracters
+var charNombersString = "0213456789";
+var lowerCaseCharsString = "abcdefghijklmnopqrstuvwxyz"
+var uppercaseCharactersString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var specialCharString = "!@#$%^&*()_+~`|}{[]\:;?><,./-="
 
-
-//  //set the passeword lenght
- 
-// //  var cuppercase = confirm("Would you like to use uppercase letters?");
-// //     var clowercase = confirm("Would you like to use lowercase letters?");
-// //     var cnumbers = confirm("would you like to use numbers?");
-// //     var csymbols = confirm("would you like to use special characters?");
-function copyPassword(){
-    document.getElementById("display").select;
-    document.execCommand("Copy")
-    alert("Passeword copy to clipboard");
-}
 
 
     
